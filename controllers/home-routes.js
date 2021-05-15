47a59ee587bf9893ddd32a3953647fbf69a42aac
unknown <baybui@ucla.edu>
2021-05-15 13:02:59 -0800

    updated conditional rendering of templates for logged in status

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,59 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment, Vote } = require("../models");
 
+//route for single-post
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      //include session login to check if user is loggedin in order to render correct template
+      res.render("single-post", { post, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//homepage route
 router.get("/", (req, res) => {
   console.log(req.session);
 
@@ -38,7 +91,10 @@ router.get("/", (req, res) => {
 
       // pass a single post object into the homepage template
 
-      res.render("homepage", { posts });
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
     })
     .catch(err => {
       console.log(err);
@@ -55,4 +111,5 @@ router.get("/login", (req, res) => {
 
   res.render("login");
 });
+
 module.exports = router;
